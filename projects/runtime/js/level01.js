b830a8ef666b09d68ee3d195790e740e7a9c95d7
charlesdouglas12 <charlesdawg388@gmail.com>
2019-12-03 18:33:18 +0000

    <runtime>

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -6,27 +6,27 @@ var level01 = function (window) {
     var createjs = window.createjs;
 
     window.opspark.runLevelInGame = function(game) {
-        // some useful constants 
+        // some useful constants
         var groundY = game.groundY;
 
         // this data will allow us to define all of the
         // behavior of our game
         var levelData = {
             name: "Robot Romp",
-            number: 1, 
+            number: 1,
             speed: -3,
             gameItems: [
                 {type: 'sawblade',x:270,y:394 },
                 {type: 'sawblade',x:700,y:394},
                 {type: 'sawblade',x:830,y:483},
                 {type: 'sawblade',x:1060,y:490}
-            
-            
+
+
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // BEGIN EDITING YOUR CODE HERE
 function createSawBlade(x,y) {
@@ -35,12 +35,12 @@ var damageFromObstacle = 10;
 var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
 myObstacle.x = x;
 myObstacle.y = y;
-game.addGameItem(myObstacle);   
+game.addGameItem(myObstacle);
 var obstacleImage = draw.bitmap('img/sawblade.png');
-myObstacle.addChild(obstacleImage); 
+myObstacle.addChild(obstacleImage);
 obstacleImage.x = -25;
 obstacleImage.y = -25;
-}  
+}
 //createSawBlade(400, 390);
 //createSawBlade(600, 460);
 //createSawBlade(800, 380);
@@ -48,17 +48,17 @@ obstacleImage.y = -25;
    for (var i = 0; i < levelData.gameItems.length; i++) {
     var gameItems = levelData.gameItems[i];
     createSawBlade(gameItems.x, gameItems.y);
-} 
-    
+}
+
     function createBox(x, y){
         var hitZoneSize = 28;
         var damageFromObstacle = 10;
         var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
         myObstacle.x = x ;
         myObstacle.y = y;
-        game.addGameItem(myObstacle);   
+        game.addGameItem(myObstacle);
         var obstacleImage = draw.bitmap("http://pngimg.com/uploads/android_logo/android_logo_PNG12.png");
-        myObstacle.addChild(obstacleImage); 
+        myObstacle.addChild(obstacleImage);
         obstacleImage.x = -25;
         obstacleImage.y = -25;
         obstacleImage.scaleX = .10;
@@ -67,8 +67,8 @@ obstacleImage.y = -25;
     createBox(1500, 380);
      createBox(2000, 380);
      createBox(2500, 490);
-    
-  
+
+
    function createEnemy(x, y) {
        var enemy =  game.createGameItem('enemy',320);
 var redSquare = draw.bitmap("http://pngimg.com/uploads/android_logo/android_logo_PNG12.png");
@@ -76,7 +76,7 @@ redSquare.x = -320;
 redSquare.y = -320;
 enemy.addChild(redSquare);
   enemy.x = x;
-enemy.y = y; 
+enemy.y = y;
    game.addGameItem(enemy);
    enemy.velocityX = -1;
    enemy.onPlayerCollision = function() {
@@ -99,7 +99,7 @@ redSquare.x = 50;
 redSquare.y = 50;
 reward.addChild(redSquare);
   reward.x = x;
-reward.y = y; 
+reward.y = y;
    game.addGameItem(reward);
    reward.velocityX = -1;
    reward.scaleX = .05;
@@ -121,9 +121,9 @@ console.log('The enemy has hit Halle');
    createReward(1200,300);
       createReward(1600,300);
    };
-   
-    
-    
+
+
+
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
